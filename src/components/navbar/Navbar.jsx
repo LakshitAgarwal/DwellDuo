f1a19aa953ec42363a0097e6ded8de4f8a9309d1
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-03-20 22:05:20 +0530

    Initial Commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+// import logo from '../../assets/logo.svg'
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      {/* logoo */}
+      <div className='nav_logo'>
+        <h1>DwellDuo</h1>
+        {/* <img src={logo} alt="Logo" /> */}
+      </div>
+      {/* links */}
+      <div className="nav_links">
+        <ul>
+          <li><a href="/findflat">Find Flats</a></li>
+          <li><a href="/signup">Find Flatmates</a></li>
+          <li><a href="/postflat">Post Flat</a></li>
+        <li><a href="/aboutus">About Us</a></li>
+         <li><a href="/FAQ">FAQs</a></li>
+        </ul>
+      </div>
+      {/* auth */}
+      <div className="nav_auth">
+        <div className="auth_button"><a href='/signup'>Signup</a>|<a href='/login'>Login</a></div>
+      </div>
+      </nav>
+  )
+}
+
+export default Navbar
